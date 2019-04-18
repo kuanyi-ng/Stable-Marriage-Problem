625307c4439b83e58c8b103c4b7cfdcd9e26c783
KY-Ng <44340199+KY-Ng@users.noreply.github.com>
2019-04-18 14:11:18 +0900

    Added assign()
    Extra:
    1. Participants.align, Active.propose and Passive.accept make up the main algorithm (Gale-Shapley algorithm)

--- stableMarriage.py
+++ stableMarriage.py
@@ -21,26 +21,96 @@ class Participants(object):
         self.pairs = {}
         
     def addReference(self, participant):
-        # register participant(Active/Passive) and his/her name in reference
+        '''register participant(Active/Passive) and his/her name in reference'''
         name = participant.name
         if name not in self.reference:
             self.reference[name] = participant
             
     def addActive(self, name, preference):
-        # create an Active object with name and its preference
-        # then add it into the reference
+        '''
+        create an Active object with name and its preference
+        then add it into the reference
+        '''
         if name not in self.reference:
             newActive = Active(name, preference)
             self.activeGroup.append(newActive)
             self.addReference(newActive)
     
-    def addPassive(self, name, preference):
-        # create an Passive object with name and its preference
-        # then add it into the reference
+    def addPassive(self, name, preference, capacity):
+        '''
+        create an Passive object with name, its preference and capacity
+        then add it into the reference
+        '''
         if name not in self.reference:
-            newPassive = Passive(name, preference)
-            self.activeGroup.append(newPassive)
+            newPassive = Passive(name, preference, capacity)
+            self.passiveGroup.append(newPassive)
             self.addReference(newPassive)
+            self.pairs[name] = []
+            
+    def updateStatus(self):
+        '''
+        update the status of Active and Passive
+        if they're in pairs(dict), status = 1; else, status = 0
+        '''
+        # update status of Active
+        pairredActive = []
+        for picked in list(self.pairs.values()):
+            pairredActive += picked
+        for active in self.activeGroup:
+            if active.name in pairredActive:
+                active.changeStatus(1)
+            else:
+                active.changeStatus(0)
+        
+        # update status of Passive
+        for passive in self.passiveGroup:
+            if self.pairs[passive.name] == []:
+                passive.changeStatus(0)
+            else:
+                passive.changeStatus(1)
+    
+    def updateFreeActive(self, count):
+        '''
+        update self.freeActive by appending Active with status = 0
+        '''
+        # reset freeActive list
+        self.freeActive = []
+        for active in self.activeGroup:
+            if active.status == 0:
+                self.freeActive.append(active.name)
+                if count > 0: # if this isn't the first time Active enters the freeActive list,
+                    active.rejectCount += 1 # it means Active is rejected by Passive in the last round
+                
+    def assign(self):
+        '''
+        Match Active with Passive
+        '''
+        count = 0 # count the number of matching(round) done
+        self.updateFreeActive(count)
+        
+        while len(self.freeActive) > 0: # when there are still Active that are not pair with any Passive
+            print("Matching Round: {}\n".format(count))
+            
+            # Active propose
+            for active in self.activeGroup:
+                if active.status == 0: # Active that are not pairred will propose
+                    active.propose(self.reference)
+            print()
+            # Passive accept
+            for passive in self.passiveGroup:
+                passive.accept(self.pairs)
+            print('\nCurrent Matching')
+            print(self.pairs)
+            
+            count += 1 # one round of matching completed
+            self.updateStatus() # update status of Active and Passive
+            self.updateFreeActive(count)
+            
+            print('unpairred:', self.freeActive)
+            print('-'*50)
+            
+        print("Stable Matching obtained!")
+        print(self.pairs)
         
 
 class Active(object):
@@ -53,16 +123,69 @@ class Active(object):
         self.name = name
         self.preference = preference
         self.status = 0
+        self.rejectCount = 0
+    
+    def changeStatus(self, changeTo):
+        self.status = changeTo
+        
+    def propose(self, reference):
+        '''
+        get the 'count'th preference of Active and
+        add Active to the Passive's candidate
+        reference: a dict to get Active using Active.name (dict)
+        count: (int)
+        '''
+        print(self.name, 'Preference:', self.preference)
+        choice = reference[self.preference[self.rejectCount]]
+        choice.candidate.append(self.name)
         
 class Passive(object):
-    def __init__(self, name, preference):
+    def __init__(self, name, preference, capacity):
         '''
         name: name of Passive (str)
         preference: {'name of Active': rank of Active} (dict: {keys(str), values(int)})
         candidate: ['name of Active that proposed to Passive'] (list of str)
         status: 0 (not pairred), 1 (pairred)
+        capacity: number of Active able to pair with (int)
         '''
         self.name = name
         self.preference = preference
         self.candidate = []
         self.status = 0
+        self.capacity = capacity
+    
+    def changeStatus(self, changeTo):
+        self.status = changeTo
+        
+    def accept(self, pairs_dict):
+        '''
+        choose Active(s) from candidate list of number up to capacity
+        pairs_dict: Participants.pairs
+        '''
+        if len(self.candidate) > 0:
+            print(self.name, 'Preference:', self.preference)
+            
+            # add temporarily pairred Active to candidate list
+            if self.status == 1:
+                self.candidate += pairs_dict[self.name]
+            print(self.name, 'Candidate:', self.candidate)
+            
+            pairs_dict[self.name] = [] # reset Passive's pair in pairs_dict 
+            
+            # a list containing only ranking (int) for easier comparison
+            candidate_rank = [self.preference[active] for active in self.candidate]
+#            print(self.name, candidate_rank) # uncommend when debugging
+            
+            # pick Active 
+            for n in range(self.capacity):
+                # get the index of choice in candidate
+                choice = min(candidate_rank)
+                choice_index = candidate_rank.index(choice)
+                # add selected Active in pairs_dict
+                pairs_dict[self.name].append(self.candidate.pop(choice_index))
+                candidate_rank.remove(choice)
+                
+        # reset candidate list after choosing
+        self.candidate = []
+        
+            
--- test-case.py
+++ test-case.py
@@ -6,26 +6,54 @@ Stable Marriage Problem with Gale-Shapley Algorithm
 
 import stableMarriage as sm
 
-village = sm.Participants()
 
-'''
-male: A, B, C, D (Passive)
-female: W, X, Y, Z (Active)
-'''
-male = [('A', {'X':1, 'Y':2, 'Z':1, 'W':4}),
-        ('B', {'X':1, 'Y':2, 'Z':1, 'W':4}), 
-        ('C', {'X':3, 'Y':1, 'Z':2, 'W':4}),
-        ('D', {'X':4, 'Y':3, 'Z':2, 'W':1})]
-
-female = [('W', ['A', 'C', 'B', 'D']), 
-          ('X', ['D', 'C', 'A', 'B']),
-          ('Y', ['A', 'D', 'C', 'B']),
-          ('Z', ['A', 'D', 'C', 'B'])]
-
-for boy in male:
-    village.addPassive(boy[0], boy[1])
-for girl in female:
-    village.addActive(girl[0], girl[1])
+def testFunction(func):
+     # initialise       
+    village = sm.Participants()
+    '''
+    male: A, B, C, D (Passive)
+    female: W, X, Y, Z (Active)
+    '''
+    male = [('A', {'X':4, 'Y':2, 'Z':1, 'W':3}, 1),
+            ('B', {'X':1, 'Y':2, 'Z':3, 'W':4}, 1), 
+            ('C', {'X':3, 'Y':1, 'Z':2, 'W':4}, 1),
+            ('D', {'X':4, 'Y':3, 'Z':2, 'W':1}, 1)]
     
-print(village.reference)
+    female = [('W', ['A', 'C', 'B', 'D']), 
+              ('X', ['D', 'C', 'A', 'B']),
+              ('Y', ['A', 'D', 'C', 'B']),
+              ('Z', ['D', 'A', 'C', 'B'])]
+    
+    for boy in male:
+        village.addPassive(boy[0], boy[1], boy[2])
+    for girl in female:
+        village.addActive(girl[0], girl[1])
+    
+    if func == 'propose':
+        for girl in village.activeGroup:
+            girl.propose(village.reference, 0)
+        for boy in village.passiveGroup:
+            print(boy.candidate)
+            
+    elif func == 'accept':
+        for girl in village.activeGroup:
+            girl.propose(village.reference, 0)
+        for boy in village.passiveGroup:
+            boy.accept(village.pairs)
+        print(village.pairs)
+    
+    elif func == 'changeStatus':
+        for girl in village.activeGroup:
+            girl.changeStatus(1)
+            print(girl.name, girl.status)
+        for girl in village.activeGroup:
+            print(girl.name, girl.status)
+            
+    elif func == 'assign':
+        village.assign()
 
+
+#testFunction('propose')
+#testFunction('accept')
+#testFunction('changeStatus')
+testFunction('assign')
